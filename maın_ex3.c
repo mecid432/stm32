f63406050990453b22aab7e75848ccd68b5012d4
majd othman <12345sat6789mo@gmail.com>
2026-05-13 11:18:54 +0300

    Create maın_ex3.c
    Özet: Bu çalışma, PC12 ve PC13 pinlerinden gelen kesmeleri EXTI15_10 hattı üzerinden yönetir. PC13 pini LED'i yakmak için, PC12 pini ise LED'i söndürmek için yapılandırılmıştır. Kesme yönetimi HAL kütüphanesinin geri çağırma (callback) mekanizması ile gerçekleştirilmiştir.

---
+++ maın_ex3.c
@@ -0,0 +1,46 @@
+ GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+ /* MCU Configuration----------------------------------------------------------*/
+ /* Reset of all peripherals, Initializes the Flash interface and the Systick. */ 
+ HAL_Init();
+ /* Configure the system clock */ 
+ SystemClock_Config();
+
+ /* GPIOA and GPIOC Configuration----------------------------------------------*/
+ /* GPIO Ports Clock Enable */ 
+ __HAL_RCC_GPIOC_CLK_ENABLE(); 
+ __HAL_RCC_GPIOA_CLK_ENABLE();
+
+ /*Configure GPIO pin : PC13 */
+ GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13; 
+ GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+ GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+ HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+ /*Configure GPIO pin : PA5 */
+ GPIO_InitStruct.Pin = GPIO_PIN_5;
+ GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; 
+ GPIO_InitStruct.Pull = GPIO_NOPULL;
+ GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW; 
+ HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+ /* Configure GPIO pin Output Level */
+ HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+ /* EXTI interrupt init*/
+ HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+ while (1);
+ }
+
+ void EXTI15_10_IRQHandler(void) {
+ HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_12); 
+ HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13); 
+ }
+
+ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) { 
+ if(GPIO_Pin == GPIO_PIN_13)
+ HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET); 
+ else if(GPIO_Pin == GPIO_PIN_12)
+ HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET); 
+ }
